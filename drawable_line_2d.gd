92669615db835b839da51b515bcfd7c9c9abc8fa
yvan <yvis99@gmail.com>
2026-03-15 15:51:49 +0100

    red draw

---
+++ drawable_line_2d.gd
@@ -0,0 +1,20 @@
+extends Line2D
+class_name drawable_line_2d
+
+const min_step = 20
+var last_point: Vector2
+
+func add_new_point(mouse_pos: Vector2) -> void:
+	if last_point != Vector2.ZERO && last_point.distance_to(mouse_pos) < min_step :
+		return
+	
+	add_point(mouse_pos)
+	last_point = mouse_pos
+
+func end_of_line() -> void:
+	if get_point_count() == 1:
+		queue_redraw()
+
+func _draw() -> void:
+	if last_point != Vector2.ZERO:
+		draw_circle(last_point, width/2, default_color)
