3d84ff44a3c30933cae9eae018c7cf149f0fd7be
yvan <yvis99@gmail.com>
2026-03-15 18:23:34 +0100

    pushable objects

---
+++ scenes/game_scene/character/object_pusher.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var pushForce: int = 1000
+
+@onready var body: CharacterBody2D = get_parent()
+
+func _physics_process(_delta) -> void:
+	if body.move_and_slide(): # true if collided
+		for i in body.get_slide_collision_count():
+			var col: KinematicCollision2D = body.get_slide_collision(i)
+			if col.get_collider() is RigidBody2D:
+				var object: RigidBody2D = col.get_collider()
+				object.apply_force(col.get_normal() * - pushForce)
