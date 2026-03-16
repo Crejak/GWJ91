c546adbfc61964dbcb2c22ce12c3d071301829ee
yvan <yvis99@gmail.com>
2026-03-16 22:29:47 +0100

    feat(door): release door

--- scenes/game_scene/environment/movable_objects/door_interactable.gd
+++ scenes/game_scene/environment/movable_objects/door_interactable.gd
@@ -1,15 +1,18 @@
 extends Node2D
 
 @onready var door: RigidBody2D = %Door
-var character: Character
 var pin_joint: PinJoint2D
 
 func _on_interactable_player_interacted(source: Character) -> void:
-	character = source
 	pin_joint = PinJoint2D.new()
-	character.add_child(pin_joint)
+	source.add_child(pin_joint)
 	pin_joint.node_a = door.get_path()
-	pin_joint.node_b = character.get_path()
+	pin_joint.node_b = source.get_path()
 	pin_joint.disable_collision = false
 	pin_joint.softness = 16
-	pin_joint.global_position = character.global_position
+	pin_joint.global_position = source.global_position
+
+
+func _on_interactable_player_stop_interaction() -> void:
+	if pin_joint:
+		pin_joint.queue_free()
