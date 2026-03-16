5555410c978fbc2337117e5362d9e669daf92be0
Valentin Rigolle <valentin.rigolle@gmail.com>
2026-03-16 14:56:14 +0100

    Character can pick up object !

--- scenes/game_scene/environment/extraction_point.gd
+++ scenes/game_scene/environment/extraction_point.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
-func _on_interactable_player_interacted() -> void:
+func _on_interactable_player_interacted(_character: Character) -> void:
 	GameState.get_current_level_state().set_phase(LevelState.Phase.CONCLUSION);
