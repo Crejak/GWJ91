71c07fc104764efae7610ac8aaeb8626aa555b67
yvan <yvis99@gmail.com>
2026-03-16 22:19:47 +0100

    fix pinJoint position

--- scenes/game_scene/environment/movable_objects/door_interactable.gd
+++ scenes/game_scene/environment/movable_objects/door_interactable.gd
@@ -7,13 +7,9 @@ var pin_joint: PinJoint2D
 func _on_interactable_player_interacted(source: Character) -> void:
 	character = source
 	pin_joint = PinJoint2D.new()
+	character.add_child(pin_joint)
 	pin_joint.node_a = door.get_path()
 	pin_joint.node_b = character.get_path()
 	pin_joint.disable_collision = false
-	pin_joint.softness = 100
+	pin_joint.softness = 16
 	pin_joint.global_position = character.global_position
-	door.add_child(pin_joint)
-
-func _process(delta: float) -> void:
-	if character && pin_joint:
-		pass
