8847216f48bf696fd65156c051862ec84722b7b6
Valentin Rigolle <valentin.rigolle@gmail.com>
2026-03-16 12:29:16 +0100

    You can configure preparation phase timer

--- scenes/game_scene/phases/preparation_phase.gd
+++ scenes/game_scene/phases/preparation_phase.gd
@@ -1,5 +1,10 @@
 extends Control
 
+## Preparation time in seconds
+@export var preparation_time: float = 8.;
+
+@export_group("Internal nodes")
+
 @export var timer: Timer;
 @export var timer_label: Label;
 
@@ -16,7 +21,7 @@ func _process(_delta: float) -> void:
 func on_phase_started(phase: LevelState.Phase) -> void:
 	if phase == LevelState.Phase.PREPARATION:
 		visible = true;
-		timer.start();
+		timer.start(preparation_time);
 
 func on_phase_ended(phase: LevelState.Phase) -> void:
 	if phase == LevelState.Phase.PREPARATION:
