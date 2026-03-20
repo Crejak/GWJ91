dce1e8ec5bd292065c52e652cf394dc4f4d7247f
Valentin Rigolle <valentin.rigolle@gmail.com>
2026-03-20 13:15:28 +0100

    Added busted effect

--- scenes/game_scene/phases/infiltration/infiltration_failure_ui.gd
+++ scenes/game_scene/phases/infiltration/infiltration_failure_ui.gd
@@ -1,10 +1,13 @@
 extends Control
 
+@onready var busted_effect: BustedEffect = %BustedEffect;
+
 func _ready() -> void:
 	SignalBus.character_caught.connect(_on_character_caught);
 	visible = false;
 
 func _on_character_caught() -> void:
 	visible = true;
+	busted_effect.start_effect();
 	await get_tree().create_timer(3).timeout;
 	GameState.get_current_level_state().set_phase(LevelState.Phase.CONCLUSION);
