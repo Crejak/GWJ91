9951a9a0e5a7b61a6f561ace23cf15f7c731e55d
yvan <yvis99@gmail.com>
2026-03-22 18:33:09 +0100

    fix: hover disable on all phase except preparation one

--- scenes/game_scene/environment/shine_on_hover.gd
+++ scenes/game_scene/environment/shine_on_hover.gd
@@ -3,6 +3,9 @@ extends PointLight2D
 var parent_area_2d: Area2D;
 
 func _process(delta: float) -> void:
+	if GameState.get_current_level_state().current_phase != LevelState.Phase.PREPARATION:
+		return
+	
 	var mouse_distance =  global_position.distance_to(get_global_mouse_position())
 	var light_power_inversed = min(mouse_distance, 100)
 	energy = abs(light_power_inversed-100) /10
