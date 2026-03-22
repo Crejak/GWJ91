f4a583bbc6eec556bdaea88010dcb5e8aa43a27d
Valentin Rigolle <valentin.rigolle@gmail.com>
2026-03-22 16:32:53 +0100

    Reduced cursor size

--- scripts/pointer_rob.gd
+++ scripts/pointer_rob.gd
@@ -9,8 +9,8 @@ func _ready() -> void:
 func on_phase_changed(in_phase: LevelState.Phase) -> void:
 	match in_phase:
 		LevelState.Phase.PREPARATION:
-			Input.set_custom_mouse_cursor(PEN_TEXTURE, 0, Vector2(66, 133))
+			Input.set_custom_mouse_cursor(PEN_TEXTURE, Input.CURSOR_ARROW, Vector2(33, 67))
 		LevelState.Phase.INFILTRATION:
-			Input.set_custom_mouse_cursor(POINTER, 0, Vector2(63, 63))
+			Input.set_custom_mouse_cursor(POINTER, Input.CURSOR_ARROW, Vector2(63, 63))
 		_:
-			Input.set_custom_mouse_cursor(null, 0, Vector2.ZERO)
+			Input.set_custom_mouse_cursor(null, Input.CURSOR_ARROW, Vector2.ZERO)
