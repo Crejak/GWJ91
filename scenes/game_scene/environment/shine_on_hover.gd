97d86aaed4ecd5af9ffdeab67bc41794bd1a8004
yvan <yvis99@gmail.com>
2026-03-22 17:58:34 +0100

    update hover emplacement in level 1

--- scenes/game_scene/environment/shine_on_hover.gd
+++ scenes/game_scene/environment/shine_on_hover.gd
@@ -1,11 +1,8 @@
-@tool
 extends PointLight2D
 
 var parent_area_2d: Area2D;
 
 func _process(delta: float) -> void:
-	if global_position.distance_to(get_global_mouse_position()) < 100:
-		print_debug("hohihoi")
-		visible = true
-	else :
-		visible = false
+	var mouse_distance =  global_position.distance_to(get_global_mouse_position())
+	var light_power_inversed = min(mouse_distance, 100)
+	energy = abs(light_power_inversed-100) /10
